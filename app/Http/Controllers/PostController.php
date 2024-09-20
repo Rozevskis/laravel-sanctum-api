c7a57e22de7240852c41fb2d95a3784063048951
unknown <you@example.com>
2024-09-20 14:10:49 +0300

    crud functionality for posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Http\Requests\StorePostRequest;
-use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -13,15 +12,21 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return $posts;
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'title' => ['required', 'max:255'],
+            'body' => 'required'
+        ]);
+        Post::create($data);
+        return $data;
     }
 
     /**
@@ -29,15 +34,22 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return $post;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, Post $post)
     {
-        //
+
+        $data = $request->validate([
+            'title' => ['required', 'max:255'],
+            'body' => 'required'
+        ]);
+
+        $post->update($data);
+        return $post;
     }
 
     /**
@@ -45,6 +57,6 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
     }
 }
